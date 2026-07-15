af53b59103b97b95e0779f66a212e4110e1c9e9e
Alexander Gonzalez Castillo <agonzalezcast@ucenfotec.ac.cr>
2026-07-14 20:06:59 -0600

    [new] archivos de las rutas creados y actualización del .env e index.js

--- CampusFest/backend/routes/visitante.route.js
+++ CampusFest/backend/routes/visitante.route.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const Certificacion = require("../models/visitante.model");
+
+//post
+router.post("/", async(req, res) => {
+    const {nombre, correo, telefono, carrera, actividades}
+    if(!nombre || !correo || !telefono || !carrera){
+		return res.status(400).json({ mensajeError: "Nombre, correo, telefono, carrera y actividades son obligatorios" });
+	}
+  
+    try {
+        const nuevoVisitante = new Visitante({ nombre, correo, telefono, carrera, actividades});
+        await nuevoVisitante.save();
+        res.status(201).json({ mensaje: "Visitante creado", visitante: nuevoVisitante });
+    } catch (error) {
+        res.status(400).json({mensajeError: error.message})
+    }
+});
