18d1d3e6e2d0fcbd8276b4939eaaacda2bfdcb10
Alexander Gonzalez Castillo <agonzalezcast@ucenfotec.ac.cr>
2026-08-10 23:25:41 -0600

    [new] Validar campos de crear y editar stand / agregar funcionalidad de modo oscuro a stands

--- CampusFest/frontend/js/darktheme.js
+++ CampusFest/frontend/js/darktheme.js
@@ -8,43 +8,96 @@ const logo = document.querySelector(".logo");
 const topbar = document.querySelector(".topbar");
 const hamburgerItems = document.querySelectorAll(".hamburger-item");
 const hamburgerBars = document.querySelectorAll(".bar");
-const botones =  document.querySelectorAll("button");
+const botones = document.querySelectorAll("button");
 const inputs = document.querySelectorAll('input');
 const labels = document.querySelectorAll('label');
 const textareas = document.querySelectorAll('textarea');
 const spans = document.querySelectorAll('span');
 
-btnTema.addEventListener("click", () => {
-    body.classList.toggle("body-dark");
-    navbarItems.forEach(item => {
-        item.classList.toggle("general-dark");
-    });
-    hamburgerItems.forEach(item => {
-        item.classList.toggle("hamburger-dark");
-    });
-    hamburgerBars.forEach(item => {
-        item.classList.toggle("bar-dark");
-    });
-    navbar.classList.toggle("general-dark");
-    logo.classList.toggle("general-dark");
-    logoContainer.classList.toggle("general-dark");
-    navbarIcons.classList.toggle("general-dark");
-    topbar.classList.toggle("navbar-shadow-dark");
-    cajaLogin.classList.toggle("login-dark");
-    cajaLogout.classList.toggle("login-dark");
-    inputs.forEach(item => {
-        item.classList.toggle("input-dark");
-    })
-    labels.forEach(item =>{
-        item.classList.toggle("general-dark");
-    })
-    botones.forEach(item =>{
-        item.classList.toggle("btn-dark");
-    })
-    textareas.forEach(item => {
-        item.classList.toggle("input-dark");
-    })
-    spans.forEach(item =>{
-        item.classList.toggle("general-dark");
-    })
-})
+
+if(localStorage.getItem("dark") == null){
+    localStorage.setItem("dark", "false");
+}
+
+toggleDark();
+toggleDark();
+
+btnTema.addEventListener("click", toggleDark)
+
+function toggleDark() {
+    if (localStorage.getItem("dark") == "false") {
+        localStorage.setItem("dark", "true");
+        body.classList.add("body-dark");
+        navbarItems.forEach(item => {
+            item.classList.add("general-dark");
+        });
+        hamburgerItems.forEach(item => {
+            item.classList.add("hamburger-dark");
+        });
+        hamburgerBars.forEach(item => {
+            item.classList.add("bar-dark");
+        });
+        navbar.classList.add("general-dark");
+        logo.classList.add("general-dark");
+        logoContainer.classList.add("general-dark");
+        navbarIcons.classList.add("general-dark");
+        topbar.classList.add("navbar-shadow-dark");
+        cajaLogin.classList.add("login-dark");
+        cajaLogout.classList.add("login-dark");
+        inputs.forEach(item => {
+            item.classList.add("input-dark");
+        })
+        labels.forEach(item => {
+            item.classList.add("general-dark");
+        })
+        botones.forEach(item => {
+            item.classList.add("btn-dark");
+        })
+        textareas.forEach(item => {
+            item.classList.add("input-dark");
+        })
+        spans.forEach(item => {
+            item.classList.add("general-dark");
+        })
+        tarjetas.forEach(item => {
+            item.classList.add("tarjeta-dark");
+        })
+    } else {
+        localStorage.setItem("dark", "false");
+        body.classList.remove("body-dark");
+        navbarItems.forEach(item => {
+            item.classList.remove("general-dark");
+        });
+        hamburgerItems.forEach(item => {
+            item.classList.remove("hamburger-dark");
+        });
+        hamburgerBars.forEach(item => {
+            item.classList.remove("bar-dark");
+        });
+        navbar.classList.remove("general-dark");
+        logo.classList.remove("general-dark");
+        logoContainer.classList.remove("general-dark");
+        navbarIcons.classList.remove("general-dark");
+        topbar.classList.remove("navbar-shadow-dark");
+        cajaLogin.classList.remove("login-dark");
+        cajaLogout.classList.remove("login-dark");
+        inputs.forEach(item => {
+            item.classList.remove("input-dark");
+        })
+        labels.forEach(item => {
+            item.classList.remove("general-dark");
+        })
+        botones.forEach(item => {
+            item.classList.remove("btn-dark");
+        })
+        textareas.forEach(item => {
+            item.classList.remove("input-dark");
+        })
+        spans.forEach(item => {
+            item.classList.remove("general-dark");
+        })
+        tarjetas.forEach(item => {
+            item.classList.remove("tarjeta-dark");
+        })
+    }
+}
